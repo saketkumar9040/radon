5dbb7f2af3339041aa44d665ac1919f50555246c
SAKET KUMAR <saketk89@gmail.com>
2022-06-03 11:00:28 +0530

    to update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,48 +1,82 @@
 const express = require('express');
 const myHelper = require('../util/helper')
 const underscore = require('underscore')
-
+const lodash= require('lodash');
+const { json } = require('express/lib/response');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+
+router.get('/movies', function(req, res){
+  let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
+  res.send(movies)
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-});
+router.get('/movies/:index', function(req, res){
+    let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
+    for (let i=0;i<=movies.length;i++){
+      if (req.params.index==i){
+        res.send(movies[i])
+      }
+       }
+    });
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-// })
+    
+router.get('/films', function(req, res){
+    let films=[
+         {
+        id: 1,
+        name: "The Shining"
+       },
+        {
+        id: 2,
+        name: "Incendies"
+       }, 
+       {
+        id: 3,
+        name: "Rang de Basanti"
+       },
+        {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       
+    res.send((lodash.union(films)))
+  });
 
-// router.get('/movies', function(req, res){
-//   let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
-//   console.log('The request objects is '+ JSON.stringify(req.params))
-//   console.log('Candidates name is '+req.params.canidatesName)
-//   res.send('Done')
-// });
 
+
+
+  router.get('/films/:Id'), function(req, res){
+    let pic=[
+      {
+        id: 1,
+        name:'king'
+  },
+  {
+    id: 2,
+    name:'dost'
+  },{
+    id:3,
+    name:'DDLJ'
+  },
+{
+  id:4,
+  name:'kite'
+},{
+  id:5,
+  name:'fan'
+}];
+for(i=0;i<pic.length;i++){
+  let fm=pic[i]
+  if(req.params.Id===i){
+    res.send(fm.name) 
+
+  }
+}
+}
+
+  
 
 module.exports = router;
 // adding this comment for no reason
