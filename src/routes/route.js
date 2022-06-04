4c97bdccdc89521a2b099282ccc12b894ade49d6
SAKET KUMAR <saketk89@gmail.com>
2022-06-04 14:00:07 +0530

    assignemnt complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,19 +6,23 @@ const { json } = require('express/lib/response');
 const router = express.Router();
 
 
-router.get('/movies', function(req, res){
-  let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
-  res.send(movies)
-});
+// router.get('/movies', function(req, res){
+//   let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
+//   res.send(movies)
+// });
 
 
 router.get('/movies/:index', function(req, res){
     let movies=['The Shawshank Redemption' ,' The Godfather','The Dark Knight', 'The Godfather: Part II ','12 Angry Men','The Lord of the Rings', 'Pulp Fiction']
     for (let i=0;i<=movies.length;i++){
-      if (req.params.index==i){
-        res.send(movies[i])
-      }
+      let index=req.params.index;
+       if(index <= movies.length && index>0){
+             res.send(movies[index])
        }
+       else{
+         res.send('no such movies in the list')
+
+       }}
     });
 
     
@@ -47,36 +51,37 @@ router.get('/films', function(req, res){
 
 
 
-  router.get('/films/:Id'), function(req, res){
-    let pic=[
+  router.get('/film/:Id', function(req, res){
+    let film=[
       {
-        'id': 1,
-        'name':'king'
+        id: 1,
+        name:'king'
   },
   {
-    'id': 2,
-    'name':'dost'
+     id: 2,
+     name:'dost'
   },{
-    'id':3,
-    'name':'DDLJ'
+     id:3,
+     name:'DDLJ'
   },
 {
-  'id':4,
- 'name':'kite'
+    id:4,
+    name:'kite'
 },{
-  'id':5,
- 'name':'fan'
-}];
-for(i=0;i<pic.length;i++){
-  let fm=pic[i]
-  if(req.params.Id===i){
-    res.send(fm.name) 
-
-  }
+   id:5,
+   name:'fan'
 }
-}
-
+]
+for(let i=0;i<=film.length;i++){
   
+let  index=req.params.Id
+if(index <=film.length && index >0){
+   res.send(film[index].name)
+   break;
+}else{
+  res.send('no such movies')
+}
+}});
 
 module.exports = router;
 // adding this comment for no reason
