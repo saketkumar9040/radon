b733f502ebb4866af3fc16c59fd5aec13f71ac26
SAKET KUMAR <saketk89@gmail.com>
2022-06-07 19:50:49 +0530

    hi

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10},
-    
-    // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
-    // {
-        // "ch1 ": "awesome intro to JS",
-        // "ch2" : "intro to nodejs",
-        // "ch3" : "intro to db"
-    //  }
-    summary :  mongoose.Schema.Types.Mixed,
-    isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
+const authorModel = new mongoose.Schema( {  
 
-}, { timestamps: true });
+        author_id:number,
+        author_name:string,
+        age:number,
+        address:string
+    } 
+, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+const bookModel = new mongoose.Schema( {  
+
+     
+    name:string,
+    author_id:number,
+    price:number,
+    ratings:number,
+
+} 
+, { timestamps: true });
+
+
+module.exports = mongoose.model('author', authorModel) //authors
+module.exports = mongoose.model('Book', bookModel) //books
