9cd3c997556c4a8acbbf26e473c37a7a70915588
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-20 16:44:30 +0530

    2nd Phase complete

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,14 +1,42 @@
 const validUrl = require("valid-url");
 const shortId = require("shortid");
 const urlModel = require("../models/urlModel");
+const redis = require("redis");
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+    13190,
+    "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////  CREATE URL API  ////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 const urlRegex = (value) => {
     let urlRegex = /^(?:(?:(?:https?|http):)?\/\/.*\.(?:png|gif|webp|com|in|org|co|co.in|net|jpeg|jpg))/i;
     if (urlRegex.test(value))
         return true;
 }
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                 /////////////////////////////////  CREATE URL API  ////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////////
 
 const createUrl = async (req, res) => {
     try {
@@ -32,19 +60,24 @@ const createUrl = async (req, res) => {
     }
 }
 
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                 /////////////////////////////////  GET URL API  ///////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////  GET URL API  ///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 const getUrl = async (req, res) => {
- try {  const urlCode = req.params.urlCode
-    if (!shortId.isValid(urlCode)) return res.status(400).send({ status: false, message: "Invalid URL!" })
-    const findUrl = await urlModel.findOne({ urlCode })
-    if(!findUrl) return res.status(400).send({ status: false, message: "Url not found!" })
-    return res.status(302).redirect(findUrl.longUrl)
-}  catch (err) {
-    return res.status(500).send({ status: false, message: err.message })
-}
+    try {
+        const urlCode = req.params.urlCode
+        if (!shortId.isValid(urlCode)) return res.status(400).send({ status: false, message: "Invalid URL!" })
+        let cache = await GET_ASYNC(`${req.params.urlCode}`)
+        cache = JSON.parse(cache)
+        if (cache) return res.status(302).redirect(cache.longUrl)
+        const findUrl = await urlModel.findOne({ urlCode })
+        if (!findUrl) return res.status(400).send({ status: false, message: "Url not found!" })
+        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(findUrl))
+        return res.status(302).redirect(findUrl.longUrl)
+    }   catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
 }
 
 module.exports = { createUrl, getUrl }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const mongoose = require('mongoose');
 const app = express();
-const multer = require('multer');
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(multer().any())
 
 route.all("/*", function (req, res) {
     res.status(400).send({
