57dcf009053b813fc24f17dff37b29cdc5ecc1fc
Sameer182002 <1978sameerbajaj@gmail.com>
2022-07-26 18:21:52 +0530

    Autho

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,6 +1,8 @@
 const jwt= require("jsonwebtoken")
-
-const auth= async function(req,res,next){
+const usermodel= require("../models/userModel")
+const { isValidObjectId } = require("../validation/validation")
+const authen= async function(req,res,next){
+    try{
      const bearerheader= req.headers.authorization
      if(typeof bearerheader=="undefined" || typeof bearerheader=="null"){
       return res.status(401).send({status:false,message:"Token Must Be present"})
@@ -10,16 +12,32 @@ const auth= async function(req,res,next){
      let token=bearerToken[1]
       let decodeToken=jwt.verify(token,"project5@sss123",{ ignoreExpiration: true },function(err,decode){
         if(err){ 
-            return res.status(400).send({status:false,message:"Token Invalid"}) }
+            return res.status(401).send({status:false,message:"Token Invalid"}) }
         else{
             if (Date.now() > decode.exp * 1000) {
-                return res.status(401).send({ status: false, message: "Session Expired" });
+                return res.status(401).send({ status: false, message: "Session Expired ! Login Again" });
             }
         }
         req.DecodedId=decode.userId
         next()
-    })
-    
+    })}
+    catch(err){
+        res.status(500).send({status:false,message:err.message})
+    }
 }
 
-module.exports={auth}
+const author= async function (req,res,next){
+    let parmId=req.params.userId
+    let dUderId=req.DecodedId
+     if(parmId){
+        if(!isValidObjectId(parmId)) return res.status(400).send({status:false,message:"Pls Enter Id in Valid Format"})
+        if(!(await usermodel.findById(parmId))) return res.status(400).send({status:false,message:"Id Doesn't Exists"})
+        if(parmId !== dUderId) return res.status(400).send({status:false,message:"Sorry You Are Not Authorise"})
+        next()
+     }
+}
+
+
+
+
+module.exports={authen,author}
--- src/route/route.js
+++ src/route/route.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const bcrypt = require("bcrypt")
-const {createUser, loginUser, getUser}=require("../controllers/userController")
-const { auth } = require("../middleware/middleware")
+const {createUser, loginUser, getUser,notFound}=require("../controllers/userController")
+const { authen,author} = require("../middleware/middleware")
 
 
 
 router.post("/register",createUser)
 router.post("/login",loginUser)
-router.get("/user/:userId/profile",auth,getUser)
-
+router.get("/user/:userId/profile",authen,author,getUser)
+ router.all("\*",notFound)
 module.exports = router
 
