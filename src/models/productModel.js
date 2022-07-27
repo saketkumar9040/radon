1a4732b4a1e0e6a079d336eae3a03c7242b58319
Sameer182002 <1978sameerbajaj@gmail.com>
2022-07-27 12:59:18 +0530

    UPDATE

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,20 +1,18 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const productSchema=new mongoose.schema({
-    
-        title: {type:String, required:true, unique:true},
-        description: {type:string, required:true},
-        price: {type:number,required:true},// valid number/decimal
-        currencyId: {type:String,required:true},// INR
-        currencyFormat: {type:String, required:true },//Rupee symbol
-        isFreeShipping: {type:boolean, default: false},
-        productImage: {type:String, required:true},  // s3 link
-        style: {type:String},
-        availableSizes: {type:[String] ,enum:["S", "XS","M","X", "L","XXL", "XL"]},
-        installments: {type:Number},
-        deletedAt: {type:Date}, 
-        isDeleted: {type:boolean, default: false}
-      
-},{timestamps:true})
+const productSchema = new mongoose.schema({
+        title: {type: String, required:true , unique:true },
+        description: { type:String, required:true },
+        price: { type:Number, required:true },
+        currencyId: { type:String, required:true },
+        currencyFormat: { type:String, required:true },
+        isFreeShipping: { type:Boolean, default: false },
+        productImage: { type:String, required:true },  // s3 link
+        style: {type: String },
+        availableSizes: { type:[String], enum:["S", "XS", "M", "X", "L", "XXL", "XL"] },
+        installments: {type:Number },
+        deletedAt: { type:Date },
+        isDeleted: { type:Boolean, default: false }
+}, { timestamps: true })
 
-module.exports=mongoose.model("product",productSchema)
+module.exports = mongoose.model("product", productSchema)
